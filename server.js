19250e6d90c48ccefd7edd758be28be298c6896f
Harish Raju <harisharaju1@gmail.com>
2020-08-09 23:40:11 +0530

    removed morgan
    removed morgan

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const morgan = require('morgan');
+//const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./database/db');
 const authRoutes = require('./routes/auth');
@@ -9,7 +9,7 @@ const itemRoutes = require('./routes/item');
 
 //middleware
 app.use(cors());
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
